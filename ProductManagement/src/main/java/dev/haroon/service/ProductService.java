0271971773ad4349f59d3a038ef5c6d250cd72bd
mohamed_haroon <moharoon11107@gmail.com>
2024-10-16 16:32:39 +0530

    DONE!

---
+++ ProductManagement/src/main/java/dev/haroon/service/ProductService.java
@@ -0,0 +1,56 @@
+package dev.haroon.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import dev.haroon.entities.Product;
+import dev.haroon.repo.ProductRepo;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepo repo;
+	
+	public List<Product> getAllProducts() {
+		return repo.findAll();
+	}
+	
+	  public Product getProductById(int id) {
+	        return repo.findById(id).orElse(null);
+	   }
+	  
+	  
+	  public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+		  product.setImageName(imageFile.getOriginalFilename());
+		  product.setImageType(imageFile.getContentType());
+		  product.setImageDate(imageFile.getBytes());
+		  return repo.save(product);
+	  }
+	  
+	  
+	 
+	   public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+		    product.setImageDate(imageFile.getBytes());
+		    product.setImageName(imageFile.getOriginalFilename());
+		    product.setImageType(imageFile.getContentType());
+		    return repo.save(product);
+	   }
+	   
+	   
+	   public void deleteProduct(int id) {
+		   repo.deleteById(id);
+	   }
+	   
+	   public List<Product> searchProduct(String keyword) {
+		   return repo.searchProducts(keyword);
+	   }
+	 
+	  
+	
+	
+}
